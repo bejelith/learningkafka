3b877bc395b71bdbeb1e18f91ec42c15d11fea81
simone.caruso <simone.caruso@workday.com>
2016-01-23 21:13:58 +0000

    Improve consumer builder, accept list in input

--- src/main/java/com/simonecaruso/kafka/consumer/ConsumerConnectorBuilder.java
+++ src/main/java/com/simonecaruso/kafka/consumer/ConsumerConnectorBuilder.java
@@ -1,8 +1,10 @@
 package com.simonecaruso.kafka.consumer;
 
 import kafka.javaapi.consumer.ConsumerConnector;
+import org.apache.commons.lang.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-
+import java.util.List;
+import java.util.LinkedList;
 import java.util.Properties;
 
 /**
@@ -10,13 +12,14 @@ import java.util.Properties;
  */
 public class ConsumerConnectorBuilder {
 
-    private String zkconnect;
-    private String boostrapservers;
-    private String topic;
-    private String gropupid;
-    private String sessionTimeout;
+    private String zkconnect = "localhost";
+    private List<String> boostrapservers = new LinkedList<>();
+    private String topic = "testtopic";
+    private String gropupid = "group1";
+    private String sessionTimeout = "100";
 
     public ConsumerConnectorBuilder() {
+
 
     }
 
@@ -25,7 +28,7 @@ public class ConsumerConnectorBuilder {
         return this;
     }
 
-    public ConsumerConnectorBuilder withBootstrapServers(String bootstrapsers) {
+    public ConsumerConnectorBuilder withBootstrapServers(List<String> bootstrapsers) {
         this.boostrapservers = boostrapservers;
         return this;
     }
@@ -40,11 +43,17 @@ public class ConsumerConnectorBuilder {
         return this;
     }
 
+    public ConsumerConnectorBuilder addServer(String server){
+        boostrapservers.add(server);
+        return this;
+    }
+
     public ConsumerConnector build() {
         Properties props = new Properties();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapservers);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, StringUtils.join(boostrapservers.iterator(), ','));
         props.put(ConsumerConfig.GROUP_ID_CONFIG, gropupid);
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS, sessionTimeout);
+        props.put("zookeeper.connect", zkconnect);
         return kafka.consumer.Consumer.createJavaConsumerConnector(new kafka.consumer.ConsumerConfig(props));
     }
 
