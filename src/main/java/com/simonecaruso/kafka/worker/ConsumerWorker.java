8315cc3997bb23961378cdb83f32af0cdfca351b
simone.caruso <simone.caruso@workday.com>
2016-01-23 21:15:31 +0000

    bug fix in while look, + simple close() strategy, more logging

--- src/main/java/com/simonecaruso/kafka/worker/ConsumerWorker.java
+++ src/main/java/com/simonecaruso/kafka/worker/ConsumerWorker.java
@@ -8,29 +8,46 @@ import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.javaapi.consumer.ConsumerConnector;
 import kafka.message.MessageAndMetadata;
+
 import java.util.List;
 
 public class ConsumerWorker implements Runnable {
 
     private final KafkaTopic topic;
     private ConsumerConnector consumer;
+    private boolean run = true;
+    private int processedmessages = 0;
 
-    public ConsumerWorker(ConsumerConnector consumer, KafkaTopic topic){
+    public ConsumerWorker(ConsumerConnector consumer, KafkaTopic topic) {
         this.consumer = consumer;
         this.topic = topic;
     }
 
     public void run() {
-        System.out.println("Consumer started.");
-        List<KafkaStream<byte[], byte[]>> streams= topic.getConsumerStreams(consumer);
-        for(KafkaStream<byte[], byte[]> stream : streams) {
-            stream.iterator();
+        System.out.println("Consumer started.");//TODO write loggin class
+        List<KafkaStream<byte[], byte[]>> streams = topic.getConsumerStreams(consumer);
+        for (KafkaStream<byte[], byte[]> stream : streams) {
             ConsumerIterator<byte[], byte[]> iterator = stream.iterator();
-            while (iterator.hasNext()) {
+            while (run && iterator.hasNext()) {
+                processedmessages++;
                 MessageAndMetadata<byte[], byte[]> message = iterator.next();
                 System.out.println("Receved: " + new String(message.key()) + ": " + new String(message.message()));
+
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    System.out.println("Interrupting");
+                    return;
+                }
             }
         }
+    }
 
+    public void exit() {
+        run = false;
+    }
+
+    public int getProcessedmessages() {
+        return processedmessages;
     }
 }
