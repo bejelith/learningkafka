8428578e650585ac60bee5bdface9c43be4fb3d7
simone.caruso <simone.caruso@workday.com>
2016-01-24 15:21:56 +0000

    integration test

--- src/test/java/com/simonecaruso/kafka/test/IntegrationTest.java
+++ src/test/java/com/simonecaruso/kafka/test/IntegrationTest.java
@@ -2,12 +2,12 @@ package com.simonecaruso.kafka.test;
 
 import com.simonecaruso.kafka.KafkaTopic;
 import com.simonecaruso.kafka.consumer.ConsumerConnectorBuilder;
+import com.simonecaruso.kafka.producer.KafkaProducerBuilder;
 import com.simonecaruso.kafka.test.embedded.EmbeddedKafkaCluster;
 import com.simonecaruso.kafka.test.embedded.EmbeddedZookeeper;
 import com.simonecaruso.kafka.worker.ConsumerWorker;
 import kafka.javaapi.consumer.ConsumerConnector;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.junit.AfterClass;
@@ -24,7 +24,7 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by simone.caruso on 23/01/2016.
  */
-public class Test1 {
+public class IntegrationTest {
 
     static EmbeddedZookeeper embeddedZookeeper;
     static EmbeddedKafkaCluster embeddedKafkaCluster;
@@ -63,37 +63,31 @@ public class Test1 {
         embeddedZookeeper.shutdown();
     }
 
-    @Test(timeout = 5000)
-    public int mytest() throws Exception {
-
+    @Test(timeout = 99000)
+    public void mytest() throws Exception {
+        ProducerRecord<String, String> record;
+        embeddedKafkaCluster.createTopics(topic.getTopicName());
         ConsumerConnector consumer = new ConsumerConnectorBuilder()
                 .addServer("localhost:" + karfkaPort)
                 .withZk("localhost:" + zookeeperPort)
+                .withSessionTimeout("50")
                 .withGroupId("g1")
                 .withTopic(topic.getTopicName())
                 .build();
+        KafkaProducer<String, String> producer = new KafkaProducerBuilder()
+                .addServer("localhost:" + karfkaPort)
+                .withClientID("producer1")
+                .build();
+
         String[] messages = new String[]{"message1", "message2", "message3"};
 
         ConsumerWorker worker = new ConsumerWorker(consumer, topic);
 
-
         thread = new Thread(worker);
-        thread.setName("testthread");
         thread.start();
+        //Access linearization
+        Thread.sleep(2500);
 
-
-
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:" + karfkaPort);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        props.put(ProducerConfig.ACKS_CONFIG, "1");
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "Producer1");
-        props.put(ProducerConfig.TIMEOUT_CONFIG, "50");
-        props.put(ProducerConfig.RETRIES_CONFIG, "1");
-
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-        ProducerRecord<String, String> record;
         for (int i = 0; i < messages.length; i++) {
             record = new ProducerRecord<>(topic.getTopicName(), Integer.toString(i), messages[i]);
             Future<RecordMetadata> f = producer.send(record);
@@ -105,9 +99,10 @@ public class Test1 {
             }
         }
 
+
         worker.exit();
         try {
-
+            Thread.sleep(1000);
             thread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
